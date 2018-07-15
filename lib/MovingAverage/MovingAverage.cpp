8e795a1bc43400ab715003cd7de32373ae98189f
Florian Bruggisser <florian@nexpose.ch>
2018-07-15 14:33:47 +0200

    fixed moving average max resolve

--- lib/MovingAverage/MovingAverage.cpp
+++ lib/MovingAverage/MovingAverage.cpp
@@ -23,38 +23,40 @@
  */
 
 #if defined(ARDUINO) && ARDUINO >= 100
+
 #include "Arduino.h"
+
 #else
 #include "WProgram.h"
 #endif
+
 #include "MovingAverage.h"
 
 MovingAverage::MovingAverage(float alphaOrN, real startValue) : _value(startValue) {
-  alphaOrN = max(alphaOrN, 0); // make sure alphaOrN >= 0
-  _alpha = (alphaOrN > 1 ?
-      2 / (alphaOrN + 1) :
-      alphaOrN);
+    //alphaOrN = max(alphaOrN, 0); // make sure alphaOrN >= 0
+    alphaOrN = alphaOrN < 0 ? 0 : alphaOrN;
+    _alpha = (alphaOrN > 1 ? 2 / (alphaOrN + 1) : alphaOrN);
 }
 
 void MovingAverage::reset(real startValue) {
-  _value = startValue;
+    _value = startValue;
 }
 
-void MovingAverage::reset(real (*valueFunc)(void))
-{
-  // Source: http://www.had2know.com/finance/exponential-moving-average-ema-calculator.html
-  // a = 2/(n+1) ==> n = 2/a - 1 ==> (n-1) / 2 = 1/a - 1
-  int n = ceil( 1.0f/_alpha - 1);
-  Serial.print("N: "); Serial.println(n);
-  real avg = 0;
-  for (int i=0; i<n; i++)
-    avg += valueFunc();
-  avg /= n;
+void MovingAverage::reset(real (*valueFunc)(void)) {
+    // Source: http://www.had2know.com/finance/exponential-moving-average-ema-calculator.html
+    // a = 2/(n+1) ==> n = 2/a - 1 ==> (n-1) / 2 = 1/a - 1
+    int n = ceil(1.0f / _alpha - 1);
+    Serial.print("N: ");
+    Serial.println(n);
+    real avg = 0;
+    for (int i = 0; i < n; i++)
+        avg += valueFunc();
+    avg /= n;
 
-  reset(avg);
+    reset(avg);
 }
 
 
 MovingAverage::real MovingAverage::update(real v) {
-  return (_value -= _alpha * (_value - v));
+    return (_value -= _alpha * (_value - v));
 }
